b887875a8d9bdc91e48601f6d6704fd54e61abf6
Evgeny Chumichev <82936400+evgnz@users.noreply.github.com>
2021-04-26 00:48:57 +0300

    indentation fix

--- CO_20_LastStand_Blufor_v_2_0.Altis/functions/scriptComponent.hpp
+++ CO_20_LastStand_Blufor_v_2_0.Altis/functions/scriptComponent.hpp
@@ -2,51 +2,51 @@
   BASIC DEFINES / MISSION SETTINGS
 --------------------------------------------------------------------------------------------------*/
 
-#define RANDOM_POS 	   		 [[13567.1,21369.4,0],[13867.9,21665.7,0],[14284.9,20855.9,0],[13925.6,21524.7,0]]
-#define POS_LIST 	   		 [[13567.1,21369.4,0],[13867.9,21665.7,0],[14284.9,20855.9,0],[13925.6,21524.7,0]]
-#define INF_POS		   		 [[13567.1,21369.4,0],[13867.9,21665.7,0],[14284.9,20855.9,0],[13925.6,21524.7,0]]
-#define ARMOR_POS	   		 [[13213,21390.1,0],[14207.3,20432.1,0],[15042.7,20979.9,3.43323e-005],[15117.4,21941.5,1.14441e-005]]
-#define TRANSPORT_POS  		 [[14164.4,21747.2,-1.52588e-005],[13295.2,21255,0],[14475.8,20496.6,0],[14898.1,21180.3,-1.52588e-005]]
-#define BASE_CRATES_POS		 [[14191.2,21205.3,0.00900269],[14223.3,21238.9,-0.000831604],[14176.2,21223.6,0],[14117.5,21276.3,0]]
+#define RANDOM_POS        [[13567.1,21369.4,0],[13867.9,21665.7,0],[14284.9,20855.9,0],[13925.6,21524.7,0]]
+#define POS_LIST 	   		  [[13567.1,21369.4,0],[13867.9,21665.7,0],[14284.9,20855.9,0],[13925.6,21524.7,0]]
+#define INF_POS		   		  [[13567.1,21369.4,0],[13867.9,21665.7,0],[14284.9,20855.9,0],[13925.6,21524.7,0]]
+#define ARMOR_POS	   		  [[13213,21390.1,0],[14207.3,20432.1,0],[15042.7,20979.9,3.43323e-005],[15117.4,21941.5,1.14441e-005]]
+#define TRANSPORT_POS  		[[14164.4,21747.2,-1.52588e-005],[13295.2,21255,0],[14475.8,20496.6,0],[14898.1,21180.3,-1.52588e-005]]
+#define BASE_CRATES_POS		[[14191.2,21205.3,0.00900269],[14223.3,21238.9,-0.000831604],[14176.2,21223.6,0],[14117.5,21276.3,0]]
 
-#define SORTED_VEHICLES	   	 ((entities "LandVehicle") - [st_1,st_2,st_3,st_4,st_5])
-#define MISSION_OBJECTS		 (entities [["CAManBase", "LandVehicle"], ["SoldierWB", "Car_F", "Tank_F", "Air"]]) select {side _x isEqualTo east}
-#define WP_HOLDERS		 	 (nearestObjects [SECTOR_POS, ["WeaponHolder", "GroundWeaponHolder", "WeaponHolderSimulated"], 1000])
-#define GARBAGE_LIST		 (WP_HOLDERS + allDead)
+#define SORTED_VEHICLES	  ((entities "LandVehicle") - [st_1,st_2,st_3,st_4,st_5])
+#define MISSION_OBJECTS		(entities [["CAManBase", "LandVehicle"], ["SoldierWB", "Car_F", "Tank_F", "Air"]]) select {side _x isEqualTo east}
+#define WP_HOLDERS		 	  (nearestObjects [SECTOR_POS, ["WeaponHolder", "GroundWeaponHolder", "WeaponHolderSimulated"], 1000])
+#define GARBAGE_LIST		  (WP_HOLDERS + allDead)
 
-#define ALIVE_PLAYERS	   	 (allUnits select {isPlayer _x})
-#define ALL_PLAYERS		 	 ((allUnits + allDeadMen) select {isPlayer _x})
-#define REMOTE_PLAYERS		 (playableUnits - [player])
-#define REMOTE_LEADER		 (ALL_PLAYERS select (ALL_PLAYERS findIf {_x isEqualTo leader group _x}))
-#define ALL_PLAYERS_NUMBER 	 (count ALL_PLAYERS)
-#define PLAYERS_NUMBER	   	 (count playableUnits)
-#define EAST_ALIVE		   	 (allUnits select {side _x isEqualTo east})
+#define ALIVE_PLAYERS	   	  (allUnits select {isPlayer _x})
+#define ALL_PLAYERS         ((allUnits + allDeadMen) select {isPlayer _x})
+#define REMOTE_PLAYERS		  (playableUnits - [player])
+#define REMOTE_LEADER       (ALL_PLAYERS select (ALL_PLAYERS findIf {_x isEqualTo leader group _x}))
+#define ALL_PLAYERS_NUMBER  (count ALL_PLAYERS)
+#define PLAYERS_NUMBER      (count playableUnits)
+#define EAST_ALIVE          (allUnits select {side _x isEqualTo east})
 
-#define PUSH_ARG(A,B) 	 	 A pushBack B
-#define RAND(A)			 	 selectRandom A
-#define CRAND(A,B,C)		 (ceil (random [A, B, C]))
-#define FRAND(A,B,C)		 (floor (random [A, B, C]))
-#define RN_RAND(A,B,C)		 (round (random [A, B, C]))
-#define INC_VAR(A,B)		 (call {A = A + B; A})
-#define DEC_VAR(A,B)		 (call {A = A - B; A})
+#define PUSH_ARG(A,B)     A pushBack B
+#define RAND(A)           selectRandom A
+#define CRAND(A,B,C)		  (ceil (random [A, B, C]))
+#define FRAND(A,B,C)      (floor (random [A, B, C]))
+#define RN_RAND(A,B,C)    (round (random [A, B, C]))
+#define INC_VAR(A,B)      (call {A = A + B; A})
+#define DEC_VAR(A,B)      (call {A = A - B; A})
 
-#define QVAR(A)			 	 (missionNamespace getVariable A)
-#define GET_VAR(A,B) 		 (missionNamespace getVariable [A, B])
-#define SET_VAR(A,B) 		 missionNamespace setVariable [A, B]
-#define SET_GVAR(A,B) 		 missionNamespace setVariable [A, B, true]
+#define QVAR(A)           (missionNamespace getVariable A)
+#define GET_VAR(A,B)      (missionNamespace getVariable [A, B])
+#define SET_VAR(A,B)      missionNamespace setVariable [A, B]
+#define SET_GVAR(A,B)     missionNamespace setVariable [A, B, true]
 
-#define GET_PARAM(A) 		 (A call (missionNamespace getVariable ["BIS_fnc_storeParamsValues_data", {}]))
-#define EVAC_ROUND	   	 	 (missionNamespace getvariable ["EvacWave", 5])
-#define SW_DELAY			 (GET_PARAM("RespawnTime") / 1.25) min 10
-#define SKIP_VALUE			 "skiptime_value"
+#define GET_PARAM(A)      (A call (missionNamespace getVariable ["BIS_fnc_storeParamsValues_data", {}]))
+#define EVAC_ROUND        (missionNamespace getvariable ["EvacWave", 5])
+#define SW_DELAY          (GET_PARAM("RespawnTime") / 1.25) min 10
+#define SKIP_VALUE        "skiptime_value"
 
-#define DIFF_INF_NUMBER		 (ceil ((ALL_PLAYERS_NUMBER / 2.25) max 4))
-#define DIFF_ARM_NUMBER	 	 ((floor (DIFF_INF_NUMBER / 2)) min 4)
-#define DIFF_TRN_NUMBER 	 (DIFF_ARM_NUMBER - 1) max 2
+#define DIFF_INF_NUMBER   (ceil ((ALL_PLAYERS_NUMBER / 2.25) max 4))
+#define DIFF_ARM_NUMBER   ((floor (DIFF_INF_NUMBER / 2)) min 4)
+#define DIFF_TRN_NUMBER   (DIFF_ARM_NUMBER - 1) max 2
 
-#define FULL_CREW(V) 		 ((fullCrew [V, "", false]) apply {_x select 0})
-#define GET_UNIT(A,B) 		 (A isEqualTo GET_VAR("JTAC_player", objNull) && {A isEqualTo B})
-#define ADDONS_LOADED 		 (isClass (configFile >> "cfgPatches" >> "rhsusf_main"))
+#define FULL_CREW(V)      ((fullCrew [V, "", false]) apply {_x select 0})
+#define GET_UNIT(A,B)     (A isEqualTo GET_VAR("JTAC_player", objNull) && {A isEqualTo B})
+#define ADDONS_LOADED     (isClass (configFile >> "cfgPatches" >> "rhsusf_main"))
 
 #define DELETE_EMPTY(A)	(call {\
   if (FULL_CREW(A) findIf {alive _x} isEqualTo -1) then {\
@@ -139,33 +139,33 @@
   UI SETTINGS 
 --------------------------------------------------------------------------------------------------*/
 
-#define CTRL_W_X			 safeZoneWAbs + safeZoneXAbs
-#define CTRL_H_Y			 safeZoneH + safeZoneY
-#define CTRL_WH 			 (safeZoneWAbs / safezoneH)
-#define CTRL_SR_XY			 (0.005 * CTRL_W_X), (0.005 * CTRL_H_Y)
-#define CTRL_EP_W 			 (17 * ((CTRL_WH min 1.2) / 80))
-#define CTRL_EP_H 			 ((((CTRL_WH min 1.2) / 1.2) / 20) * 0.85)
-#define CTRL_EP_X 			 (safeZoneXAbs + 1 * ((CTRL_WH min 1.2) / 40))
-#define CTRL_EP_Y 			 (safezoneY + safezoneH - 27 * (((CTRL_WH min 1.2) / 1.2) / 25))
-#define CTRL_SKIP			 [0.396875 * CTRL_W_X, 0.731 * CTRL_H_Y, 0.20625 * safeZoneWAbs, 0.02485 * safezoneH]
+#define CTRL_W_X      safeZoneWAbs + safeZoneXAbs
+#define CTRL_H_Y      safeZoneH + safeZoneY
+#define CTRL_WH       (safeZoneWAbs / safezoneH)
+#define CTRL_SR_XY    (0.005 * CTRL_W_X), (0.005 * CTRL_H_Y)
+#define CTRL_EP_W 		(17 * ((CTRL_WH min 1.2) / 80))
+#define CTRL_EP_H 		((((CTRL_WH min 1.2) / 1.2) / 20) * 0.85)
+#define CTRL_EP_X 		(safeZoneXAbs + 1 * ((CTRL_WH min 1.2) / 40))
+#define CTRL_EP_Y 		(safezoneY + safezoneH - 27 * (((CTRL_WH min 1.2) / 1.2) / 25))
+#define CTRL_SKIP     [0.396875 * CTRL_W_X, 0.731 * CTRL_H_Y, 0.20625 * safeZoneWAbs, 0.02485 * safezoneH]
 
 #define CTRL_W 				 1.75  
 #define CTRL_H 				 1.15  
 #define CTRL_X 				 ((safeZoneXAbs + (safeZoneWAbs / 2)) - (CTRL_W / 2))  
 #define CTRL_Y 				 ((safeZoneY + safeZoneH) - CTRL_H)
 
-#define MISSION_DISPLAY	 	 (findDisplay 46)
-#define DISPLAY_STATUS 		 (uiNamespace getVariable ["RscMissionStatus", displayNull])
-#define DISPLAY_COMMMENU 	 (uiNamespace getVariable ["RscCommMenuItems", displayNull])
+#define MISSION_DISPLAY      (findDisplay 46)
+#define DISPLAY_STATUS 		   (uiNamespace getVariable ["RscMissionStatus", displayNull])
+#define DISPLAY_COMMMENU 	   (uiNamespace getVariable ["RscCommMenuItems", displayNull])
 #define RSC_LAYER_PROGRESS	 ("RscMissionStatus" call BIS_fnc_rscLayer)
 
-#define CTRL_FONT_H			 0.035
-#define CTRL_B_COLOR		 [0, 0, 0, 1]
-#define CTRL_FONT			 "PuristaMedium"
-#define CTRL_INT_LOGO		 "\a3\Ui_f\data\GUI\Cfg\Hints\CompetitiveRules_ca.paa"
-#define CTRL_INT_IMAGE		 "\a3\ui_f\data\GUI\Rsc\RscDisplayWelcome\welcome_nexus3_ca.paa"
-#define CTRL_ICON_X			 "\a3\Ui_f\data\GUI\Rsc\RscDisplayArcadeMap\icon_exit_cross_ca.paa"
-#define CTRL_ICON_BI		 "\a3\ui_f\data\GUI\RscCommon\RscDebugConsole\forum_ca.paa"
+#define CTRL_FONT_H			  0.035
+#define CTRL_B_COLOR		  [0, 0, 0, 1]
+#define CTRL_FONT			    "PuristaMedium"
+#define CTRL_INT_LOGO		  "\a3\Ui_f\data\GUI\Cfg\Hints\CompetitiveRules_ca.paa"
+#define CTRL_INT_IMAGE    "\a3\ui_f\data\GUI\Rsc\RscDisplayWelcome\welcome_nexus3_ca.paa"
+#define CTRL_ICON_X			  "\a3\Ui_f\data\GUI\Rsc\RscDisplayArcadeMap\icon_exit_cross_ca.paa"
+#define CTRL_ICON_BI		  "\a3\ui_f\data\GUI\RscCommon\RscDebugConsole\forum_ca.paa"
 
 #define CTRL_INT_ICONS (\
   "<img image='\a3\ui_f\data\GUI\Cfg\CommunicationMenu\casheli_ca.paa' align='left' valign='true'/>" + "<br/>" +\
@@ -178,7 +178,7 @@
 #define CTRL_TEXT_TPOS		 localize "STR_text_grid_target" 
 #define CTRL_TEXT_RPOS		 localize "STR_text_grid_FO"	 
 #define CTRL_TEXT_CHECK		 localize "STR_key_checkFire"	 
-#define CTRL_TT_CLOSE		 localize "STR_key_close"				
+#define CTRL_TT_CLOSE      localize "STR_key_close"				
 #define CTRL_TT_TEXT_LMB	 localize "STR_key_toolTip" 
 
 #define CTRL_HEADER_TEXT	 format ["<t align='left' size='1' font='PuristaMedium' color='#ffffff'>%1</t>", localize "STR_key_header"] 
@@ -189,33 +189,33 @@
 #define CTRL_FSUPP_TEXT		 format ["<t align='left' size='0.85' font='PuristaMedium'>%1</t>",localize "STR_fire_support"] + "<br/>"		
 #define CTRL_CALLS_TEXT		 format ["<t align='left' size='0.85' font='PuristaMedium'>%1 %2</t>",localize "STR_calls_support",GET_VAR("calls_left",5)]
 
-#define CTRL_CALL_ICON		 "<img image='\a3\ui_f\data\gui\cfg\Hints\CallSupport_CA.paa' align='left' size='1.88'/>"
-#define CTRL_ICON_LINES		 "<t align='left' size='1.15' font='PuristaLight' color='#a8a8a8'>ııllı</t>"
+#define CTRL_CALL_ICON    "<img image='\a3\ui_f\data\gui\cfg\Hints\CallSupport_CA.paa' align='left' size='1.88'/>"
+#define CTRL_ICON_LINES   "<t align='left' size='1.15' font='PuristaLight' color='#a8a8a8'>ııllı</t>"
 
-#define HINT_TEXT_UNITS		 format ["<t align='left' font='PuristaMedium'>%1</t>",localize "STR_rep_units"]  
-#define HINT_TEXT_ROUNDS	 format ["<t align='left' font='PuristaMedium'>%1</t>",localize "STR_rep_rounds"]  
-#define HINT_TEXT_NROUNDS	 format ["<t align='left' font='PuristaMedium'>%1</t>",localize "STR_rep_roundsN"] 
-#define HINT_TEXT_DELAY		 format ["<t align='left' font='PuristaMedium'>%1</t>",localize "STR_rep_delay"]    
-#define HINT_TEXT_ETA		 format ["<t align='left' font='PuristaMedium'>%1</t>",localize "STR_rep_eta"] 
-#define HINT_ICON_TEXT		 format ["<t align='left' font='PuristaMedium'>%1</t>",localize "STR_req_received"] 
+#define HINT_TEXT_UNITS   format ["<t align='left' font='PuristaMedium'>%1</t>",localize "STR_rep_units"]  
+#define HINT_TEXT_ROUNDS  format ["<t align='left' font='PuristaMedium'>%1</t>",localize "STR_rep_rounds"]  
+#define HINT_TEXT_NROUNDS format ["<t align='left' font='PuristaMedium'>%1</t>",localize "STR_rep_roundsN"] 
+#define HINT_TEXT_DELAY		format ["<t align='left' font='PuristaMedium'>%1</t>",localize "STR_rep_delay"]    
+#define HINT_TEXT_ETA     format ["<t align='left' font='PuristaMedium'>%1</t>",localize "STR_rep_eta"] 
+#define HINT_ICON_TEXT    format ["<t align='left' font='PuristaMedium'>%1</t>",localize "STR_req_received"] 
 
-#define HINT_ICON_UNITS      "<img image='\a3\ui_f\data\GUI\Cfg\CommunicationMenu\artillery_ca.paa' align='left' size='1.15'/>"
-#define HINT_TEXT_GRID		 "<t align='left' font='PuristaMedium'>%1</t>"		
+#define HINT_ICON_UNITS   "<img image='\a3\ui_f\data\GUI\Cfg\CommunicationMenu\artillery_ca.paa' align='left' size='1.15'/>"
+#define HINT_TEXT_GRID    "<t align='left' font='PuristaMedium'>%1</t>"		
 
 #define SCORCHER_NAME		 (getText (configFile >> "CfgVehicles" >> "B_T_MBT_01_arty_F" >> "DisplayName"))
 #define MORTAR_NAME			 (getText (configFile >> "CfgVehicles" >> "B_T_Mortar_01_F" >> "DisplayName"))
 
-#define LB_UNITS_LIST		 [SCORCHER_NAME,MORTAR_NAME]  
-#define LB_ROUNS_LIST_A		 ["155 mm HE","155 mm DPICM"]
-#define LB_ROUNS_LIST_B		 ["82 mm HE","82 mm Smoke","82 mm ILLUM"]
-#define LB_NUMBER_LIST_A	 ["1","2","3","4","5","6","7","8","9","10"]
-#define LB_NUMBER_LIST_B	 ["1"]
-#define LB_DELAY_LIST		 ["5","10","15","20","25","30","35","40"]
+#define LB_UNITS_LIST     [SCORCHER_NAME,MORTAR_NAME]  
+#define LB_ROUNS_LIST_A   ["155 mm HE","155 mm DPICM"]
+#define LB_ROUNS_LIST_B   ["82 mm HE","82 mm Smoke","82 mm ILLUM"]
+#define LB_NUMBER_LIST_A  ["1","2","3","4","5","6","7","8","9","10"]
+#define LB_NUMBER_LIST_B  ["1"]
+#define LB_DELAY_LIST     ["5","10","15","20","25","30","35","40"]
 
-#define PROFILE_RGB_R		 (profilenamespace getvariable ["GUI_BCG_RGB_R", 0.13])
-#define PROFILE_RGB_G		 (profilenamespace getvariable ["GUI_BCG_RGB_G", 0.54])
-#define PROFILE_RGB_B		 (profilenamespace getvariable ["GUI_BCG_RGB_B", 0.21])
-#define PROFILE_RGB_A		 (profilenamespace getvariable ["GUI_BCG_RGB_A", 0.8])
+#define PROFILE_RGB_R    (profilenamespace getvariable ["GUI_BCG_RGB_R", 0.13])
+#define PROFILE_RGB_G    (profilenamespace getvariable ["GUI_BCG_RGB_G", 0.54])
+#define PROFILE_RGB_B    (profilenamespace getvariable ["GUI_BCG_RGB_B", 0.21])
+#define PROFILE_RGB_A    (profilenamespace getvariable ["GUI_BCG_RGB_A", 0.8])
 
 #define LB_ROUNDS_ADD(A,B) (call {\
   lnbClear A;\
@@ -292,34 +292,34 @@
   ICONS / MARKERS 
 --------------------------------------------------------------------------------------------------*/
 
-#define SIZE_F			0.035
-#define NAME_X			(name _x)
-#define VEH_X			(vehicle _x)
-#define DIR_X			(getDir VEH_X)
-#define POS_X			(visiblePosition VEH_X)
-#define NAME_DRIVER		(name driver VEH_X)
-#define GET_CREW(A) 	count ((crew A) select {alive _x})
-#define GET_NAME		[NAME_DRIVER, ""] select (isNull driver VEH_X)
-#define ICON_SHAPE 		getText (configfile >> "CfgVehicles" >> typeOf VEH_X >> "icon")
-#define DISPLAY_NAME	getText (configfile >> "CfgVehicles" >> typeOf VEH_X >> "displayName")
-#define DNAME_SHORT(A)	format ["%1.%2", [A, "displayNameShort"] call BIS_fnc_rankParams, NAME_X]
-#define ICON_RANK(A)	format ["a3\Ui_f\data\GUI\Cfg\Ranks\%1_gs.paa", rank A]
+#define SIZE_F          0.035
+#define NAME_X          (name _x)
+#define VEH_X           (vehicle _x)
+#define DIR_X           (getDir VEH_X)
+#define POS_X           (visiblePosition VEH_X)
+#define NAME_DRIVER     (name driver VEH_X)
+#define GET_CREW(A)     count ((crew A) select {alive _x})
+#define GET_NAME        [NAME_DRIVER, ""] select (isNull driver VEH_X)
+#define ICON_SHAPE      getText (configfile >> "CfgVehicles" >> typeOf VEH_X >> "icon")
+#define DISPLAY_NAME    getText (configfile >> "CfgVehicles" >> typeOf VEH_X >> "displayName")
+#define DNAME_SHORT(A)  format ["%1.%2", [A, "displayNameShort"] call BIS_fnc_rankParams, NAME_X]
+#define ICON_RANK(A)	  format ["a3\Ui_f\data\GUI\Cfg\Ranks\%1_gs.paa", rank A]
 
-#define POS_ADV(A)		A modelToWorldVisual [0, 0, ((A selectionPosition "head") select 2) + 0.5]
-#define POS_SHORT(A)	A modelToWorldVisual [0, 0, ((A selectionPosition "head") select 2) - 0.35]
+#define POS_ADV(A)    A modelToWorldVisual [0, 0, ((A selectionPosition "head") select 2) + 0.5]
+#define POS_SHORT(A)  A modelToWorldVisual [0, 0, ((A selectionPosition "head") select 2) - 0.35]
 
-#define FONT_M 	 		"PuristaMedium"
-#define FONT_L			"PuristaLight"
-#define INCAP_COLOR		[0.4,0,0,0.55]
-#define EAST_COLOR		[0.5,0,0,1]
-#define WEST_COLOR 		[0,0.3,0.5,0.85]
-#define GUER_COLOR 		[0,0.5,0,1]
-#define CIV_COLOR 		[0.5,0.5,0.5,1]
-#define XX_COLOR 		[1,1,1,1]
+#define FONT_M        "PuristaMedium"
+#define FONT_L        "PuristaLight"
+#define INCAP_COLOR   [0.4,0,0,0.55]
+#define EAST_COLOR    [0.5,0,0,1]
+#define WEST_COLOR    [0,0.3,0.5,0.85]
+#define GUER_COLOR    [0,0.5,0,1]
+#define CIV_COLOR     [0.5,0.5,0.5,1]
+#define XX_COLOR      [1,1,1,1]
 
-#define ALL_SIDES 		[east,west,independent,civilian,sideUnknown]
-#define GET_COLOR(A) 	[WEST_COLOR,INCAP_COLOR] select (lifeState A == "INCAPACITATED")
-#define FIND_COLOR(A) 	[EAST_COLOR,WEST_COLOR,GUER_COLOR,CIV_COLOR,XX_COLOR] select (ALL_SIDES find (side group A))
+#define ALL_SIDES     [east,west,independent,civilian,sideUnknown]
+#define GET_COLOR(A)  [WEST_COLOR,INCAP_COLOR] select (lifeState A == "INCAPACITATED")
+#define FIND_COLOR(A) [EAST_COLOR,WEST_COLOR,GUER_COLOR,CIV_COLOR,XX_COLOR] select (ALL_SIDES find (side group A))
 
 #define INTRO_ICONS [\
   ["\a3\ui_f\data\Map\Markers\NATO\respawn_inf_ca", WEST_COLOR, [14110.3,21256.3,0], 0.65, 0.65, 0, "", 0],\
@@ -337,25 +337,25 @@
   MODULE SECTOR 
 --------------------------------------------------------------------------------------------------*/
 
-#define SECTOR_OWNER_CHANGED 	"_this call SL_fnc_sectorCaptured"
-#define SECTOR_NAME				localize "STR_sector_name"
-#define SECTOR_SIGN 			localize "STR_sector_ID"
-#define SECTOR_POS	   			[14176.5,21236.3,0]
-#define SECTOR_AREA				[30, 25, 0, false]
-#define SECTOR_SIDES			[east,west]
-#define SECTOR_OWNER			1
-#define SECTOR_REWARD			50
-#define SECTOR_TASK_OWNER		0
-#define SECTOR_TASK_TITLE		""
-#define SECTOR_TASK_DESCR		""
-#define SECTOR_OWNER_LIMIT		1
-#define COST_INFANTRY			0.25
-#define COST_WHEELED			0.25
-#define COST_TRACKED			0.45
-#define COST_WATER				0
-#define COST_AIR				0
-#define COST_PLAYERS			"2.25" 
-#define CAP_COEFICENT			0.05
+#define SECTOR_OWNER_CHANGED  "_this call SL_fnc_sectorCaptured"
+#define SECTOR_NAME           localize "STR_sector_name"
+#define SECTOR_SIGN           localize "STR_sector_ID"
+#define SECTOR_POS            [14176.5,21236.3,0]
+#define SECTOR_AREA           [30, 25, 0, false]
+#define SECTOR_SIDES          [east,west]
+#define SECTOR_OWNER          1
+#define SECTOR_REWARD         50
+#define SECTOR_TASK_OWNER     0
+#define SECTOR_TASK_TITLE     ""
+#define SECTOR_TASK_DESCR     ""
+#define SECTOR_OWNER_LIMIT    1
+#define COST_INFANTRY         0.25
+#define COST_WHEELED          0.25
+#define COST_TRACKED          0.45
+#define COST_WATER            0
+#define COST_AIR              0
+#define COST_PLAYERS          "2.25" 
+#define CAP_COEFICENT         0.05
 
 #define SET_SECTOR(A) (call {\
   A setVariable ["Name", SECTOR_NAME];\
@@ -385,11 +385,11 @@
   TASKS TITLES / DESCRIPTIONS / SUPPORTS MESSAGES
 --------------------------------------------------------------------------------------------------*/
 
-#define TASK_LWAVE_DESC 	["STR_lastwave_desc", "STR_lastwave_title", "STR_evac_marker"]			
-#define TASK_EVAC_DESC		["STR_evac_desc", "STR_evac_title", "STR_evac_marker"]	
-#define TASK_CALL_DESC		["STR_callheli_desc", "STR_callheli_title", ""]
-#define TASK_WAVE_DESC		"STR_wave_desc"
-#define TASK_WAVE_TITLE		"STR_wave_title"
+#define TASK_LWAVE_DESC   ["STR_lastwave_desc", "STR_lastwave_title", "STR_evac_marker"]			
+#define TASK_EVAC_DESC    ["STR_evac_desc", "STR_evac_title", "STR_evac_marker"]	
+#define TASK_CALL_DESC    ["STR_callheli_desc", "STR_callheli_title", ""]
+#define TASK_WAVE_DESC    "STR_wave_desc"
+#define TASK_WAVE_TITLE   "STR_wave_title"
 
 #define SUPP_MESSAGE_LIST [\
   "","STR_request","STR_supporting","STR_supported","STR_support_rtb",\
